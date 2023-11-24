bbac4f5e9ec265bf46d43fae663d4cecb90a3ce2
kevinguyen76 <62038441+kevinguyen76@users.noreply.github.com>
2023-11-24 22:57:16 +1100

    Added new card btn with basic functionality

--- Module 3 Making interactive websites/Build a Blackjack game/index.js
+++ Module 3 Making interactive websites/Build a Blackjack game/index.js
@@ -1,11 +1,21 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 2;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let cardsArr = [firstCard, secondCard];
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: " + cardsArr[0] + " " + cardsArr[1];
+  sumEl.textContent = "Sum: " + sum;
+
   if (sum <= 20) {
     message.textContent = "Do you want to draw a new card? 🙂";
   } else if (sum === 21) {
@@ -17,4 +27,15 @@ function startGame() {
   }
 }
 
-biggerThan21(sum);
+function newCard() {
+  console.log("Do you want to draw a new card?");
+
+  // 1. Create a card variable, and hard code its value to a number (2-11)
+  let extraCard = 9;
+
+  // 2. Add the new card to the sum variable
+  sum += extraCard;
+
+  // 3. Call startGame()
+  renderGame();
+}
