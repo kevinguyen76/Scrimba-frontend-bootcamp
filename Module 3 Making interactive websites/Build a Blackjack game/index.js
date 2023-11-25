ccaa5f723e830281fcb346ca69ed8398f8372e96
kevinguyen76 <62038441+kevinguyen76@users.noreply.github.com>
2023-11-25 12:27:13 +1100

    Completed blackjack mini game

--- Module 3 Making interactive websites/Build a Blackjack game/index.js
+++ Module 3 Making interactive websites/Build a Blackjack game/index.js
@@ -1,19 +1,53 @@
-let firstCard = 10;
-let secondCard = 2;
-let sum = firstCard + secondCard;
+let sum = 0;
+let cardsArr = [];
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
-let cardsArr = [firstCard, secondCard];
+let playerEl = document.getElementById("player-el");
+
+let player = {
+  name: "Kevin",
+  chips: 888,
+};
+
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function startGame() {
-  renderGame();
+  isAlive = true;
+
+  if (isAlive && cardsArr.length === 0) {
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cardsArr.push(firstCard);
+    cardsArr.push(secondCard);
+    sum = firstCard + secondCard;
+
+    renderGame();
+  } else {
+    cardsArr.length = 0;
+    startGame();
+  }
+}
+
+function getRandomCard() {
+  let randomNumer = Math.floor(Math.random() * 13) + 1;
+  if (randomNumer > 10) {
+    return 10;
+  } else if (randomNumer === 1) {
+    return 11;
+  } else {
+    return randomNumer;
+  }
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: " + cardsArr[0] + " " + cardsArr[1];
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cardsArr.length; i++) {
+    cardsEl.textContent += cardsArr[i] + " ";
+  }
+
   sumEl.textContent = "Sum: " + sum;
 
   if (sum <= 20) {
@@ -28,14 +62,10 @@ function renderGame() {
 }
 
 function newCard() {
-  console.log("Do you want to draw a new card?");
-
-  // 1. Create a card variable, and hard code its value to a number (2-11)
-  let extraCard = 9;
-
-  // 2. Add the new card to the sum variable
-  sum += extraCard;
-
-  // 3. Call startGame()
-  renderGame();
+  if (isAlive && !hasBlackJack) {
+    let card = getRandomCard();
+    sum += card;
+    cardsArr.push(card);
+    renderGame();
+  }
 }
